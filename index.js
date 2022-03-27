fe252faedfa48c4fb900c6462d24dabffde9aa32
Michael Simmons <msimmons86@gmail.com>
2022-03-27 16:58:43 -0700

    commit tree e3dbc00a98c34d36ad709d19ca45d373e6f5fefd

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+ return cats.push(name);
+}
+
+function destructivelyPrependCat(catName) {
+  return cats.unshift(catName)
+}
+
+function destructivelyRemoveLastCat() {
+  return cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+  return cats.shift()
+}
+
+function appendCat(catCopy) {
+  return catCopy = [...cats, "Broom"]
+}
+
+function prependCat(copyCat) {
+  return copyCat = ["Arnold", ...cats]
+}
+
+function destructivelyRemoveLastCat(removeLast) {
+  removeLast = cats.splice(-1)
+}
+
+function removeLastCat(carefullyRemoveLastCat) {
+  return carefullyRemoveLastCat = cats.slice(0, 2)
+}
+
+function removeFirstCat(carefullyRemoveFirstCat) {
+  return carefullyRemoveFirstCat = cats.slice(1)
+}
